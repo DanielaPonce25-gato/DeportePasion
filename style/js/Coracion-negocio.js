76763ed723a326948ce7dcd649875ea12c47f53b
DanielaPonce25-gato <estudiosdanielaponce25@gmail.com>
2025-07-02 17:34:07 -0300

    carga de productos-falta media 801 hasta 1400px

--- style/js/Coracion-negocio.js
+++ style/js/Coracion-negocio.js
@@ -2,21 +2,27 @@
     let totalGeneral = 0;
     let productosSeleccionados = [];
 
+    /* El método getElementById() es una función del objeto document que nos permite 
+      obtener una referencia a un elemento HTML específico en el DOM mediante su atributo id.
+      
+      Este método devuelve una referencia al nodo del DOM, lo que nos permite 
+      acceder y modificar sus propiedades.*/
+
     let resultadoElement = document.getElementById("resultado"); // visializa los productos generados en el html 
     let listaProductos = document.getElementById("lista-productos"); // visualiza los productos que seleciono el usuarrio en el carrito en el html
     let carritoVisible = false;  
 
     let productos = [   //Array de productos generados 
-      { nombre: "Pantalon Nike Negro", precio: 10.99, imagen: "pantalon-nike-negro.jpg" },
-      { nombre: "Zapatillas Azul", precio: 5.99, imagen: "adidas-zapazul.jpg" },
-      { nombre: "Patalon corto Negro", precio: 15.49, imagen: "pantalon-cortonegro.jpg" },
-      { nombre: "Termo Azul", precio: 5.99, imagen: "termo-azul.jpg" },
-      { nombre: "Termo Negro", precio: 15.49, imagen: "termo-negro.jpg" },
-      { nombre: "Pantalon Nike Azul", precio: 5.99, imagen: "pantalon-nike-azul.jpg" },
-      { nombre: "Pantalon Nike Gris", precio: 15.49, imagen: "pantalon-nike-gris.jpg" },
-      { nombre: "Patalon corto Gris", precio: 5.99, imagen: "pantalon-corto-gris.jpg" },
-      { nombre: "Producto 3", precio: 15.49, imagen: "pantalon-corto-azul.jpg" },
-      { nombre: "Producto 2", precio: 5.99, imagen: "imagen3.jpg" },
+      { nombre: "Pantalon Nike Negro", precio: 10000.99, imagen: "pantalon-nike-negro.jpg" },
+      { nombre: "Zapatilla Azul", precio: 50000.45, imagen: "adidas-zapazul.jpg" },
+      { nombre: "Patalon corto Negro", precio: 5000.33, imagen: "pantalon-cortonegro.jpg" },
+      { nombre: "Termo Azul", precio: 3000.50, imagen: "termo-azul.jpg"},
+      { nombre: "Termo Azul", precio: 3000.50, imagen: "termo-negro.jpg"},
+      { nombre: "Pantalon Nike Azul", precio: 12000.40, imagen: "pantalon-nike-azul.jpg" },
+      { nombre: "Pantalon Nike Gris", precio: 14000.25, imagen: "pantalon-nike-gris.jpg" },
+      { nombre: "Patalon corto Gris", precio: 8000.17, imagen: "pantalon-corto-gris.jpg" },
+      { nombre: "Zapatilla de correr Negra", precio: 43000.30, imagen: "zapatillas-correr-negra.jpg" },
+      { nombre: "Zapatilla de correr celeste", precio: 52000.30, imagen: "zapatillas-correr-celeste.jpg" },
       { nombre: "Producto 3", precio: 15.49, imagen: "imagen3.jpg" },
       { nombre: "Producto 2", precio: 5.99, imagen: "imagen3.jpg" },
       { nombre: "Producto 3", precio: 15.49, imagen: "imagen3.jpg" },
@@ -26,18 +32,19 @@
 
     function mostrarProductos() { //Funcion de mostrar producto selecionado y el boton agregar al carrito
       let productosHTML = "";
-      productos.forEach((producto, index) => {
+      productos.forEach((producto, index) => {  // forEach() para iterar sobre los elementos de un array  
+      /* toFixed se utiliza para redondear un número a una cantidad */
       productosHTML += `
       <div class="producto">
         <h2>${producto.nombre}</h2>
-        <p>Precio: $${producto.precio.toFixed(2)}</p>
+        <p>Precio: $${producto.precio.toFixed(2)}</p> 
         <img src="./style/imagen/productos/${producto.imagen}" alt="${producto.nombre}" width="100">
         <br>
         <button onclick="agregarCarrito(${index})">Agregar al carrito</button>
       </div>
       `;
       });
-      resultadoElement.innerHTML = productosHTML;
+      resultadoElement.innerHTML = productosHTML;  // La innerHTML , propiedad  que establece o devuelve el contenido HTML (HTML interno) de un elemento.
     } 
 
     function agregarCarrito(index) {  //Función de hacer la suma de los productos selecionados e incorporar el descuento
@@ -50,7 +57,7 @@
       });
       alert(`Producto agregado: ${producto.nombre}`); // aviso de que producto agregado
       actualizarCarrito();
-      document.getElementById("contador-carrito").textContent = productosSeleccionados.length; // cueta producto
+      document.getElementById("contador-carrito").textContent = productosSeleccionados.length; // cueta producto  // longitud exacta
     }
 
     function actualizarCarrito() { // propósito actualizar visualmente el contenido del carrito de compras en la página 
@@ -60,12 +67,14 @@
 
       productosSeleccionados.forEach(produc => { //forEach recorre Array y ejecuta la función
 
-        const item = document.createElement("li"); //Genera un nuevo item a la lista de carriro. 
+        const item = document.createElement("li");//Genera un nuevo item a la lista de carriro. // createElement () crea un elemento HTML
         item.innerHTML = `<span>${produc.nombre} - $${produc.precio.toFixed(2)}</span>`;
 
-        listaProductos.appendChild(item); //Lo agrega al contenedor de la lista del carrito.
+
+        listaProductos.appendChild(item); //Lo agrega al contenedor de la lista del carrito. // appendchild para adjuntar elementos a un elemento padre
+
       });
-      document.getElementById("total").textContent = `Total: $${totalGeneral}`;
+      document.getElementById("total").textContent = `Total: $${totalGeneral}`; // textContent  acceder y modificar el contenido a texto puro del elemento HTML
     }
 
     function cancelar() {
@@ -121,3 +130,6 @@
     });
 
     mostrarProductos();  // Donde comiensa la acción. inovamos la función mostrarProductos
+
+
+
